3e88e43367513e8dc7a16d8fc0450007c4f4d4a5
Aram Hamo <contact@aramhamo.me>
2024-02-16 07:45:04 +0100

    new file:   src/content/.gitignore new file:   src/index.php

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+<body>
+  <center><h1>Upload</h1></center>
+  <form class="form-control form" method="post" enctype="multipart/form-data">
+    <input name="files[]" multiple=multiple type="file" />
+    <input name="submit" type="submit" value="upload" />
+  </form>
+</body>
+<html>
+<?php
+ini_set('upload_max_filesize', '8192M');
+ini_set('max_file_uploads', '500');
+ini_set('post_max_size', '8192M');
+
+
+if(isset($_POST['submit']) ){
+  $filesCount = count($_FILES['files']['name']);
+  $songs = count($_FILES['files']['name']);
+
+  for($i = 0;$i < $filesCount ;$i++){
+    $fileName = $_FILES["files"]["name"][$i];
+    $file = file_get_contents($_FILES['files']['tmp_name'][$i]);
+    move_uploaded_file($_FILES['files']['tmp_name'][$i], 'content/'.$fileName);
+  }
+}
